a6d6b13561d4beded2f512c5433ebb6f7b6d3940
Dmitry Korochkin <abramslost@gmail.com>
2023-04-14 15:31:06 +0300

    add accordion

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,11 @@
 //form
 
-const forms = document.querySelectorAll('.form__element');
-const inputMask = new Inputmask('+7 (999) 999-99-99');
+const forms = document.querySelectorAll(".form__element");
+const inputMask = new Inputmask("+7 (999) 999-99-99");
 
 forms.forEach(form => {
   const telSelector = form.querySelector('input[type="tel"]');
-  
+
   inputMask.mask(telSelector);
 
   new window.JustValidate(".form__element", {
@@ -37,229 +37,47 @@ forms.forEach(form => {
   });
 });
 
+//accordion
+
+let accordion = document.querySelector(".answerblock__items");
+let tab = document.querySelectorAll(".answerblock__item");
+let answer = document.querySelectorAll(".answerblock__item-text");
+let plus = document.querySelectorAll(".answerblock__icon");
+let close = document.querySelectorAll(".answerblock__icon-close");
+
+accordion.addEventListener("click", e => {
+  const target = e.target.closest(".answerblock__item");
+
+  if (target) {
+    tab.forEach((item, index) => {
+      if (item === target && target.classList.toggle("active")) {
+        answer[index].classList.add("active");
+        tab[index].classList.add("answerblock__item-active");
+        plus[index].style.display = "none";
+        close[index].style.display = "block";
+      } else {
+        answer[index].classList.remove("active");
+        tab[index].classList.remove("answerblock__item-active");
+        plus[index].style.display = "block";
+        close[index].style.display = "none";
+      }
+    });
+  }
+});
+
+
+// tabs 
+
+const itemLeft = document.querySelectorAll(".shooting__video-item");
+const img = document.querySelectorAll(".shooting__video-play");
+
+itemLeft.forEach((item, index) => {
+  item.addEventListener("mouseover", () => {
+    img.forEach(i => {
+      i.classList.remove("active");
+    });
+    img[index].classList.add("active");
+  });
+});
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const form_one = document.querySelector('.form__element');
-// const form_two = document.querySelector('.form__element2');
-// const form_three = document.querySelector('.form__element3');
-// const form_four = document.querySelector('.form__element4');
-
-// const telSelector = form_one.querySelector('input[type="tel"]');
-// const telSelector2 = form_two.querySelector('input[type="tel"]');
-// const telSelector3 = form_three.querySelector('input[type="tel"]');
-// const telSelector4 = form_four.querySelector('input[type="tel"]');
-
-// const inputMask = new Inputmask("+7 (999) 999-99-99");
-
-// inputMask.mask(telSelector);
-// inputMask.mask(telSelector2);
-// inputMask.mask(telSelector3);
-// inputMask.mask(telSelector4);
-
-// new window.JustValidate(".form__element", {
-//   rules: {
-//     tel: {
-//       required: true,
-//       function: () => {
-//         const phone = telSelector.inputmask.unmaskedvalue();
-//         return Number(phone) && phone.length === 10;
-//       }
-//     }
-//   },
-//   submitHandler: function(thisForm) {
-//     let formData = new FormData(thisForm);
-//     let xhr = new XMLHttpRequest();
-
-//     xhr.onreadystatechange = function() {
-//       if (xhr.readyState === 4) {
-//         if (xhr.status === 200) {
-//           console.log("Отправлено");
-//         }
-//       }
-//     };
-//     xhr.open("POST", "mail.php", true);
-//     xhr.send(formData);
-
-//     thisForm.reset();
-//   }
-// });
-
-// new window.JustValidate(".form__element2", {
-//   rules: {
-//     tel: {
-//       required: true,
-//       function: () => {
-//         const phone = telSelector2.inputmask.unmaskedvalue();
-//         return Number(phone) && phone.length === 10;
-//       }
-//     }
-//   },
-//   submitHandler: function(thisForm) {
-//     let formData = new FormData(thisForm);
-//     let xhr = new XMLHttpRequest();
-
-//     xhr.onreadystatechange = function() {
-//       if (xhr.readyState === 4) {
-//         if (xhr.status === 200) {
-//           console.log("Отправлено");
-//         }
-//       }
-//     };
-//     xhr.open("POST", "mail.php", true);
-//     xhr.send(formData);
-
-//     thisForm.reset();
-//   }
-// });
-
-// new window.JustValidate(".form__element3", {
-//   rules: {
-//     tel: {
-//       required: true,
-//       function: () => {
-//         const phone = telSelector2.inputmask.unmaskedvalue();
-//         return Number(phone) && phone.length === 10;
-//       }
-//     }
-//   },
-//   submitHandler: function(thisForm) {
-//     let formData = new FormData(thisForm);
-//     let xhr = new XMLHttpRequest();
-
-//     xhr.onreadystatechange = function() {
-//       if (xhr.readyState === 4) {
-//         if (xhr.status === 200) {
-//           console.log("Отправлено");
-//         }
-//       }
-//     };
-//     xhr.open("POST", "mail.php", true);
-//     xhr.send(formData);
-
-//     thisForm.reset();
-//   }
-// });
-
-// new window.JustValidate(".form__element4", {
-//   rules: {
-//     tel: {
-//       required: true,
-//       function: () => {
-//         const phone = telSelector2.inputmask.unmaskedvalue();
-//         return Number(phone) && phone.length === 10;
-//       }
-//     }
-//   },
-//   submitHandler: function(thisForm) {
-//     let formData = new FormData(thisForm);
-//     let xhr = new XMLHttpRequest();
-
-//     xhr.onreadystatechange = function() {
-//       if (xhr.readyState === 4) {
-//         if (xhr.status === 200) {
-//           console.log("Отправлено");
-//         }
-//       }
-//     };
-//     xhr.open("POST", "mail.php", true);
-//     xhr.send(formData);
-
-//     thisForm.reset();
-//   }
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const forms = document.querySelectorAll(".form__element");
-// const input = document.querySelectorAll("input");
-
-// const message = {
-//   loading: "Загрузка...",
-//   success: "Спасибо мы с вами свяжемся",
-//   failure: "Что то пошло не так..."
-// };
-
-// const postData = async (url, data) => {
-//   document.querySelector('.status').textContent = message.loading
-//   let result = await fetch(url, {
-//     method: 'POST',
-//     body: data
-//   });
-
-//   return await result.text()
-// }
-
-// const clearInputs = () => {
-//   input.forEach(input => {
-//     input.value = '';
-//   })
-// }
-
-// forms.forEach(form => {
-//   form.addEventListener("submit", (e) => {
-//     e.preventDefault();
-
-//     let statusMessage = document.createElement("div");
-//     statusMessage.classList.add('status');
-//     form.appendChild(statusMessage);
-
-//     const formData = new FormData(form);
-
-//     postData('server.php', formData)
-//     .then(result => {
-//       console.log(result)
-//       statusMessage.textContent = message.success;
-//     })
-//     .catch(() => statusMessage.textContent = message.failure)
-//     .finally(() => {
-//       clearInputs();
-//       setTimeout(() => {
-//         statusMessage.remove();
-//       }, 5000);
-//     })
-//   });
-// });
